dadf4d45735633160cd7660b7e70241ef344b5e7
Olga_Bryzhak <obrizhak2016@gmail.com>
2025-08-22 19:09:11 +0300

    js-hw-07

---
+++ js/task-3.js
@@ -0,0 +1,6 @@
+const inputEl = document.querySelector(`#name-input`);
+const outputEl = document.querySelector(`#name-output`);
+inputEl.addEventListener(`input`, event => {
+const name = event.currentTarget.value.trim();
+outputEl.textContent = name ===``? `Anonymos`:name;
+});
